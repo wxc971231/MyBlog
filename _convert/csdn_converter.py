20cdccac2e21e74a2eac1f5a9aff51f20a2651aa
“wxc971231” <wwxc971231@163.com>
2025-11-02 18:52:50 +0800

    小目标检测的尺寸极限

--- _convert/csdn_converter.py
+++ _convert/csdn_converter.py
@@ -510,8 +510,8 @@ description:
 
 def main():
     # 配置文件路径和URL
-    markdown_file = r"D:\Programmer\Hexo\_convert\raw\数学杂烩\Wasserstein 距离简介.md"
-    article_url = "https://blog.csdn.net/wxc971231/article/details/153417678"
+    markdown_file = r"D:\Programmer\Hexo\_convert\raw\数学杂烩\小目标检测的尺寸极限.md"
+    article_url = "https://blog.csdn.net/wxc971231/article/details/151802535"
     
     # 创建转换器
     converter = CSDNConverter()
